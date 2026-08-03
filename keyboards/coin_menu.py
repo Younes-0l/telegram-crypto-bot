b233f7488d5c6dd365a3f3df016b4e95e8e5b4b1
Younes <younes2525.det@gmail.com>
2026-08-03 12:14:09 +0330

    feat: add get_coin_selection_menu

---
+++ keyboards/coin_menu.py
@@ -0,0 +1,11 @@
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def get_coin_selection_menu():
+    keyboard = [
+        [InlineKeyboardButton("BTC - بیت کوین", callback_data="coin:BTC"), InlineKeyboardButton("ETH - اتریوم", callback_data="coin:ETH")],
+        [InlineKeyboardButton("SOL - سولانا", callback_data="coin:SOL"), InlineKeyboardButton("USDT - تتر", callback_data="coin:USDT")],
+        [InlineKeyboardButton("TON - تون کوین", callback_data="coin:TON"), InlineKeyboardButton("NOT - نات کوین", callback_data="coin:NOT")],
+        [InlineKeyboardButton("ADA - کاردانو", callback_data="coin:ADA"), InlineKeyboardButton("TRX - ترون", callback_data="coin:TRX")],
+    ]
+    return InlineKeyboardMarkup(keyboard)
