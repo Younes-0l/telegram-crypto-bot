f9342d5c126fda96fee4b797b01636882fc9cd78
Younes <younes2525.det@gmail.com>
2026-08-03 10:36:51 +0330

    fix: bug fix for start

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, filters, MessageHandler, CallbackQueryHandler
 from telegram import Update
 from handlers.settings import BOT_TOKEN
+from handlers import start
 
 
 def main():
     print("Starting Bot ...")
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", ))
+    app.add_handler(CommandHandler("start", start.start))
     app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
